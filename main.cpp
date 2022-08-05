6ea936a9b2ced89cd0910ba00a1d5cb134078663
mwtam <ming.tam.w@outlook.com>
2022-08-05 15:03:18 +0800

    update the test cases in main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <vector>
 #include "webcmp_tools.h"
 
-int main(int, char**) {
+int main(int, char **)
+{
     std::cout << "Hello, world! From main\n";
-    std::cout << download_page("https://example.com") << "\n";
-    // std::cout << download_page("https://example.comxxx") << "\n";
+
+    std::vector v = {
+        "https://example.com",
+        "https://example.comxxx",
+    };
+
+    BrowserCurl b;
+
+    for (auto &&url : v)
+    {
+        std::cout << "Visiting: " << url << "\n";
+        if (b.go(url) > 0)
+        {
+            std::cout << b.page << "\n";
+        }
+        else
+        {
+            std::cout << "Something's wrong: " << b.error_msg << "\n";
+        }
+    }
 }
